59ef242fe105faa8316a88e5d0a1f956e71c7115
prairiepavement <84539524+prairiepavement@users.noreply.github.com>
2021-07-28 18:34:39 -0500

    2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,4 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const WeatherIcons = ({ weatherList }) =>
+    weatherList.map((weatherItem) => {
+        const iconSrc = "http://openweathermap.org/img/wn/" + weatherItem.icon + "@2x.png"
+        return <p key={weatherItem.icon}>{weatherItem.main} <br /><img alt="Weather icon" src={iconSrc} /></p>
+    })
+
+const Weather = ({ country }) => {
+
+    const [weather, setWeather] = useState('');
+
+    const api_key = process.env.REACT_APP_API_KEY
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${country.capital},${country.alpha3Code}&units=metric&appid=${api_key}`
+
+    useEffect(() => {
+        axios
+        .get(url)
+        .then(response => {
+            setWeather(response.data)
+        })
+    }, [url])
+
+    if (weather === '') {
+        return <p>No weather info</p>
+    }
+    if (weather !== '')
+
+    return <>
+        <h3>Weather in {country.capital}</h3>
+        <WeatherIcons weatherList={weather.weather} />
+        <p>Temperature: {weather.main.temp} &deg;C</p>
+        <p>min: {weather.main.temp_min} &deg;C max: {weather.main.temp_max} &deg;C </p>
+        <p>Humidity: {weather.main.humidity}%</p>
+        <p>Wind: {weather.wind.speed} km/h</p>
+    </>
+
+}
 
 const LanguageList = ({ languages }) => 
     languages.map((language) => <li key={language.iso639_1}>{language.name}</li>)
@@ -13,6 +51,7 @@ const Country = ({ country }) =>
         <LanguageList languages={country.languages} />
     </ul>
     <img src={country.flag} alt="Flag" width="300" />
+    <Weather country={country} />
     </>
 
 export default Country
